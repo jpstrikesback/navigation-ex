b951e613d881924d81d6fbf29479d18090823879
satyajit.happy <satyajit.happy@gmail.com>
2019-09-17 16:25:21 +0200

    wip

--- packages/example/src/Screens/BottomTabs.tsx
+++ packages/example/src/Screens/BottomTabs.tsx
@@ -1,10 +1,10 @@
 /* eslint-disable import/namespace, import/default */
 
 import * as React from 'react';
-import { MaterialIcons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 // @ts-ignore
-import TouchableBounce from 'react-native/Libraries/Components/Touchable/TouchableBounce';
+// import TouchableBounce from 'react-native/Libraries/Components/Touchable/TouchableBounce';
 import Albums from '../Shared/Albums';
 import Contacts from '../Shared/Contacts';
 import Chat from '../Shared/Chat';
@@ -17,7 +17,11 @@ const getTabBarIcon = (name: string) => ({
   tintColor: string;
   horizontal: boolean;
 }) => (
-  <MaterialIcons name={name} color={tintColor} size={horizontal ? 17 : 24} />
+  <MaterialCommunityIcons
+    name={name}
+    color={tintColor}
+    size={horizontal ? 17 : 24}
+  />
 );
 
 type BottomTabParams = {
@@ -26,6 +30,8 @@ type BottomTabParams = {
   contacts: undefined;
   chat: undefined;
 };
+
+const TouchableBounce = undefined;
 
 const BottomTabs = createBottomTabNavigator<BottomTabParams>();
 
---
+++ packages/example/webpack.config.js
@@ -0,0 +1,51 @@
+/* eslint-disable import/no-commonjs */
+
+const path = require('path');
+const fs = require('fs');
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
+
+module.exports = async function(env, argv) {
+  const config = await createExpoWebpackConfigAsync(env, argv);
+
+  config.module.rules.push({
+    test: /packages\/.+\.(js|ts|tsx)$/,
+    use: 'babel-loader',
+  });
+
+  config.resolve.plugins = config.resolve.plugins.filter(
+    p => !(p instanceof ModuleScopePlugin)
+  );
+
+  config.resolve.alias['react'] = path.resolve(
+    __dirname,
+    'node_modules',
+    'react'
+  );
+  config.resolve.alias['react-native'] = path.resolve(
+    __dirname,
+    'node_modules',
+    'react-native-web'
+  );
+  config.resolve.alias['react-native-web'] = path.resolve(
+    __dirname,
+    'node_modules',
+    'react-native-web'
+  );
+
+  config.resolve.alias[
+    '@expo/vector-icons/MaterialCommunityIcons'
+  ] = require.resolve('@expo/vector-icons/MaterialCommunityIcons');
+
+  fs.readdirSync(path.join(__dirname, '..')).forEach(name => {
+    config.resolve.alias[`@react-navigation/${name}`] = path.resolve(
+      __dirname,
+      '..',
+      name,
+      'src'
+    );
+  });
+
+  return config;
+};
